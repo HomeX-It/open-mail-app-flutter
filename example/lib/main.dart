19c4263c1a6795ea2c0b6778ade783d3043ef96c
Jason Rai <jrai@homex.com>
2020-05-08 08:50:28 -0400

    example update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,37 +5,85 @@ import 'package:flutter/services.dart';
 import 'package:open_mail_app/open_mail_app.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Open Mail App Example"),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              RaisedButton(
-                child: Text("Open Mail App!"),
-                onPressed: () {
-                  OpenMailApp.openMailApp();
-                },
-              ),
-              RaisedButton(
-                child: Text("Get Mail Apps"),
-                onPressed: () {
-                  OpenMailApp.getMailApps();
-                },
-              ),
-            ],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Open Mail App Example"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Open Mail App"),
+              onPressed: () async {
+                var result = await OpenMailApp.openMailApp();
+                if (!result) {
+                  showNoMailAppsDialog(context);
+                }
+              },
+            ),
+            RaisedButton(
+              child: Text("Get Mail Apps"),
+              onPressed: () async {
+                var apps = await OpenMailApp.getMailApps();
+                if (apps.length == 0) {
+                  showNoMailAppsDialog(context);
+                } else {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Text("Mail Apps"),
+                        content: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            for (var app in apps) Text(app.name),
+                          ],
+                        ),
+                        actions: <Widget>[
+                          FlatButton(
+                            child: Text("OK"),
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                          )
+                        ],
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  void showNoMailAppsDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Open Mail App"),
+          content: Text("No mail apps installed"),
+          actions: <Widget>[
+            FlatButton(
+              child: Text("OK"),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            )
+          ],
+        );
+      },
     );
   }
 }
